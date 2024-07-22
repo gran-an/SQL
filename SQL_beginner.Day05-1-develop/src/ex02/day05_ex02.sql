7b006c7d93280e5101f83aa45a6cf2730118f4ea
Ann Gorobets <135361149+gran-an@users.noreply.github.com>
2024-07-22 12:33:45 +0300

    Delete SQL_beginner.Day05-1-develop directory

--- SQL_beginner.Day05-1-develop/src/ex02/day05_ex02.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX idx_person_name ON person (UPPER(name));
-
-SET ENABLE_SEQSCAN TO OFF;
-
-EXPLAIN ANALYZE
-SELECT *
-FROM person
-WHERE UPPER(name) = 'IRINA';
--- SQL_beginner.Day05-1-develop/src/ex04/day05_ex04.sql
+++
@@ -1,8 +0,0 @@
-CREATE UNIQUE INDEX idx_menu_unique ON menu (pizzeria_id, pizza_name);
-
-SET ENABLE_SEQSCAN TO OFF;
-
-EXPLAIN ANALYZE
-SELECT *
-FROM menu
-WHERE pizzeria_id = 1 AND pizza_name = 'cheese pizza';
